ffa43071b9de7b95de1c8fb7574ef0c37c213b36
Andriamanitra <mikko77792@gmail.com>
2022-12-01 07:04:09 +0200

    solve day 1

---
+++ day01/solution.rb
@@ -0,0 +1,17 @@
+require_relative '../lib/base.rb'
+
+
+class Day01 < AdventOfCode
+  def self.take_input(s)
+    s.split("\n\n").map{_1.split.map(&:to_i)}
+  end
+
+  part(1) do |input|
+    input.map(&:sum).max
+  end
+  part(2) do |input|
+    input.map(&:sum).max(3).sum
+  end
+end
+
+Day01.run
