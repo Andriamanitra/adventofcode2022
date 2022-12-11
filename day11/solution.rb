abb18503d49994c969d5141d14e02488cdb527fb
Andriamanitra <mikko77792@gmail.com>
2022-12-11 07:50:42 +0200

    solve day 11 in Ruby

---
+++ day11/solution.rb
@@ -0,0 +1,73 @@
+require_relative '../lib/base.rb'
+
+class Monkey
+  def initialize(a, b, c, d, e, f)
+    @num = a.split[1].to_i
+    @items = b.split[2..].map(&:to_i)
+    @op = c.split('new = ')[1]
+    @modulo = d[/\d+/].to_i
+    @true_throw_to = e[/\d+/].to_i
+    @false_throw_to = f[/\d+/].to_i
+    @num_inspected = 0
+  end
+
+  def take_turn(worry_divisor, &block)
+    until @items.empty?
+      old = @items.shift
+      @num_inspected += 1
+      worry = eval(@op) / worry_divisor
+      if worry % @modulo == 0
+        block.call(worry, @true_throw_to)
+      else
+        block.call(worry, @false_throw_to)
+      end
+    end
+  end
+
+  def receive_item(item)
+    @items.push(item)
+  end
+
+  def modulo
+    @modulo
+  end
+
+  def num_inspects
+    @num_inspected
+  end
+end
+
+class Day11 < AdventOfCode
+  def self.take_input(s)
+    s.split("\n\n").map{_1.lines}
+  end
+
+  part(1) do |input|
+    monkeys = input.map{Monkey.new(*_1)}
+    20.times do
+      monkeys.each do |monkey|
+        monkey.take_turn(3) do |worry, target|
+          monkeys[target].receive_item(worry)
+        end
+      end
+    end
+    a, b = monkeys.map(&:num_inspects).max(2)
+    a * b
+  end
+
+  part(2) do |input|
+    monkeys = input.map{Monkey.new(*_1)}
+    mod = monkeys.map(&:modulo).reduce{_1 * _2}
+    10000.times do
+      monkeys.each do |monkey|
+        monkey.take_turn(1) do |worry, target|
+          monkeys[target].receive_item(worry % mod)
+        end
+      end
+    end
+    a, b = monkeys.map(&:num_inspects).max(2)
+    a * b
+  end
+end
+
+Day11.run
