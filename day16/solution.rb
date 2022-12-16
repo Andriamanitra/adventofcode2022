eadb4edf8905da41573e779928d64b29f038af63
Andriamanitra <mikko77792@gmail.com>
2022-12-16 09:44:47 +0200

    much faster part 1 by only pathfinding to valves that can be opened

--- day16/solution.rb
+++ day16/solution.rb
@@ -6,44 +6,53 @@ class Day16 < AdventOfCode
       .map{[_1, _2.to_i, _3.split(', ')]}
   end
 
-  part('1 (slow)') do |input|
+  def self.bfs(map, a, b)
+    q = [[a, 0]]
+    visited = Set[]
+    until q.empty?
+      curr, steps = q.shift
+      return steps if curr == b
+      map[curr].each do |nextnode|
+        q << [nextnode, steps + 1] if visited.add?(nextnode)
+      end
+    end
+    raise "no path from #{a} to #{b}"
+  end
+
+  part(1) do |input|
     map = {}
     flows = {}
     input.each do |name, flowrate, tunnels|
-      map[name] = tunnels.map{[_1, 1]}
+      map[name] = tunnels
       flows[name] = flowrate
     end
 
-    # remove nodes that don't have a flow
-    flows.each do |k, flow|
-      if k != 'AA' && flow == 0
-        case map[k]
-        in [[a, ad], [b, bd]]
-          map[a].map!{|c, cd| c == k ? [b, ad+bd] : [c, cd]}
-          map[b].map!{|c, cd| c == k ? [a, ad+bd] : [c, cd]}
-          map.delete(k)
+    distance_between = {}
+
+    map.keys.combination(2) do |a, b|
+      dist = bfs(map, a, b)
+      distance_between[a+b] = dist
+      distance_between[b+a] = dist
+    end
+
+    useful_valves = flows.keys.select{|k| flows[k] > 0}
+
+    best = 0
+    q = [['AA', useful_valves.to_set, 30, 0]]
+
+    until q.empty?
+      me, closed_valves, t, score = q.shift
+      best = score if score > best
+      closed_valves.each do |valve|
+        dist = distance_between[me+valve]
+        if dist < t - 1
+          remaining_time = t - dist - 1
+          new_score = score + flows[valve] * remaining_time
+          q << [valve, closed_valves.dup.delete(valve), remaining_time, new_score]
         end
       end
     end
 
-    best = 0
-    visited = Set[]
-    q = [['AA', Set[], 30, 0, 0]]
-    until q.empty?
-      me, opens, t, score, income = q.shift
-      if !opens.include?(me) && flows[me] > 0
-        q << [me, opens.dup.add(me), t-1, score + income, income + flows[me]]
-      end
-      map[me].each do |dest, dist|
-        if dist < t
-          nextstate = [dest, opens, t-dist, score + income*dist, income]
-          q << nextstate if visited.add?(nextstate)
-        else
-          total_score = score + income*t
-          best = total_score if total_score > best
-        end
-      end
-    end
     best
   end
 end
